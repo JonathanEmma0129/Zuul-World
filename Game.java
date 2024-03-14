ff5d78f3c9787aff7d209ed9b0df91a0e8c57e5a
Jonathan J.S <126605271+JonathanEmma0129@users.noreply.github.com>
2024-03-13 21:59:09 -0600

    Zuul-World-v1.2
    Se añadieron 4 habitaciones (La cafetería, El gimnasio, el sótano y el estacionamiento), se implemento la dirección up y down, para cambiar de piso en el mapa.

--- Game.java
+++ Game.java
@@ -1,84 +1,83 @@
 /**
+ * This class is part of the "World of Zuul" application. 
+ * "World of Zuul" is a very simple, text-based adventure game.  
+ *
  * @author  Michael Kolling and David J. Barnes
  * @version 1.0 (February 2002)
  */
 
-public class Game 
-{
+public class Game {
     private Parser parser;
     private Room currentRoom;
-        
+
     /**
      * Create the game and initialise its internal map.
      */
-    public Game() 
-    {
+    public Game() {
         createRooms();
         parser = new Parser();
     }
 
-    /**
+    /**upstairs Exit
      * Create all the rooms and link their exits together.
      */
-    private void createRooms()
-    {
-        Room outside, theatre, pub, lab, office;
-      
-        // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theatre = new Room("in a lecture theatre");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
-        
-        // initialise room exits
-        outside.setExits(null, theatre, lab, pub);
-        theatre.setExits(null, null, null, outside);
-        pub.setExits(null, outside, null, null);
-        lab.setExits(outside, office, null, null);
-        office.setExits(null, null, null, lab);
+    private void createRooms() {
+        Room fuera, conferencias, bar, lab, oficina, cafeteria, gimnasio, bodega, estacionamiento;
 
-        currentRoom = outside;  // start game outside
+        // create the rooms
+        fuera = new Room("fuera de la entrada principal de la universidad");
+        conferencias = new Room("en una sala de conferencias");
+        bar = new Room("en el bar del campus");
+        lab = new Room("en un laboratorio de computacion");
+        oficina = new Room("en la oficina de administración informatica");
+        cafeteria = new Room("en la cafeteria de la universidad");
+        gimnasio = new Room("en el gimnasio de la universidad");
+        bodega = new Room("en en el bodega universidad");
+        estacionamiento = new Room("en el estacionamiento de la universidad");
+
+        // initialise room exits
+        fuera.setExits(null, conferencias, lab, bar, cafeteria, estacionamiento);
+        conferencias.setExits(null, null, null, fuera, null, null);
+        bar.setExits(null, fuera, null, null, null, null);
+        lab.setExits(fuera, oficina, null, null, null, null);
+        oficina.setExits(null, null, null, lab, null, null);
+        cafeteria.setExits(null, gimnasio, null, null, null, fuera);
+        gimnasio.setExits(null, null, null, cafeteria, null, null);
+        bodega.setExits(null, null, estacionamiento, null, null, null);
+        estacionamiento.setExits(bodega, null, null, null, fuera, null);
+
+        //north-east-south-west--up-down 
+
+        currentRoom = fuera;  // start game fuera
     }
 
     /**
-     *  Main play routine.  Loops until end of play.
+     * Main play routine. Loops until end of play.
      */
-    public void play() 
-    {            
+    public void play() {
         printWelcome();
 
-        // Enter the main command loop.  Here we repeatedly read commands and
+        // Enter the main command loop. Here we repeatedly read commands and
         // execute them until the game is over.
-                
+
         boolean finished = false;
-        while (! finished) {
+        while (!finished) {
             Command command = parser.getCommand();
             finished = processCommand(command);
         }
-        System.out.println("Thank you for playing.  Good bye.");
+        System.out.println("Gracias por jugar. Adios.");
     }
 
     /**
      * Print out the opening message for the player.
      */
-    private void printWelcome()
-    {
+    private void printWelcome() {
         System.out.println();
-        System.out.println("Welcome to the World of Zuul!");
-        System.out.println("Type 'help' if you need help.");
+        System.out.println("Bienvenido al mundo de Zuul!");
+        System.out.println("Escriba help si necesita ayuda.");
         System.out.println();
-        System.out.println("You are " + currentRoom.getDescription());
-        System.out.print("Exits: ");
-        if(currentRoom.northExit != null)
-            System.out.print("north ");
-        if(currentRoom.eastExit != null)
-            System.out.print("east ");
-        if(currentRoom.southExit != null)
-            System.out.print("south ");
-        if(currentRoom.westExit != null)
-            System.out.print("west ");
-        System.out.println();
+        System.out.println("Estas " + currentRoom.getDescription());
+        System.out.println("Salidas disponibles: north, east, south, west, up, down");
     }
 
     /**
@@ -86,12 +85,11 @@ public class Game
      * If this command ends the game, true is returned, otherwise false is
      * returned.
      */
-    private boolean processCommand(Command command) 
-    {
+    private boolean processCommand(Command command) {
         boolean wantToQuit = false;
 
-        if(command.isUnknown()) {
-            System.out.println("I don't know what you mean...");
+        if (command.isUnknown()) {
+            System.out.println("No se a que te refieres...");
             return false;
         }
 
@@ -106,31 +104,29 @@ public class Game
         return wantToQuit;
     }
 
-    // implementations of user commands:
+    // Implementations of user commands:
 
     /**
      * Print out some help information.
-     * Here we print some stupid, cryptic message and a list of the 
+     * Here we print some stupid, cryptic message and a list of the
      * command words.
      */
-    private void printHelp() 
-    {
-        System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
+    private void printHelp() {
+        System.out.println("Estas perdido. Usted esta solo. tu deambulas");
+        System.out.println("alrededor de la universidad.");
         System.out.println();
-        System.out.println("Your command words are:");
+        System.out.println("Tus palabras de comando son:");
         System.out.println("   go quit help");
     }
 
-    /** 
+    /**
      * Try to go to one direction. If there is an exit, enter
      * the new room, otherwise print an error message.
      */
-    private void goRoom(Command command) 
-    {
-        if(!command.hasSecondWord()) {
+    private void goRoom(Command command) {
+        if (!command.hasSecondWord()) {
             // if there is no second word, we don't know where to go...
-            System.out.println("Go where?");
+            System.out.println("Go a donde?");
             return;
         }
 
@@ -138,45 +134,38 @@ public class Game
 
         // Try to leave current room.
         Room nextRoom = null;
-        if(direction.equals("north"))
+        if (direction.equals("north"))
             nextRoom = currentRoom.northExit;
-        if(direction.equals("east"))
+        else if (direction.equals("east"))
             nextRoom = currentRoom.eastExit;
-        if(direction.equals("south"))
+        else if (direction.equals("south"))
             nextRoom = currentRoom.southExit;
-        if(direction.equals("west"))
+        else if (direction.equals("west"))
             nextRoom = currentRoom.westExit;
+        else if (direction.equals("up"))
+            nextRoom = currentRoom.upExit;
+        else if (direction.equals("down"))
+            nextRoom = currentRoom.downExit;
 
         if (nextRoom == null)
-            System.out.println("There is no door!");
+            System.out.println("No hay puerta!");
         else {
             currentRoom = nextRoom;
-            System.out.println("You are " + currentRoom.getDescription());
-            System.out.print("Exits: ");
-            if(currentRoom.northExit != null)
-                System.out.print("north ");
-            if(currentRoom.eastExit != null)
-                System.out.print("east ");
-            if(currentRoom.southExit != null)
-                System.out.print("south ");
-            if(currentRoom.westExit != null)
-                System.out.print("west ");
-            System.out.println();
+            System.out.println("Estas " + currentRoom.getDescription());
+            System.out.println("Salidas disponibles: north, east, south, west, up, down");
         }
     }
 
-    /** 
+    /**
      * "Quit" was entered. Check the rest of the command to see
      * whether we really quit the game. Return true, if this command
      * quits the game, false otherwise.
      */
-    private boolean quit(Command command) 
-    {
-        if(command.hasSecondWord()) {
-            System.out.println("Quit what?");
+    private boolean quit(Command command) {
+        if (command.hasSecondWord()) {
+            System.out.println("Quit que?");
             return false;
-        }
-        else
+        } else
             return true;  // signal that we want to quit
     }
 }
--- Room.java
+++ Room.java
@@ -1,28 +1,28 @@
-/*
+/**
  * Class Room - a room in an adventure game.
  *
  * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text based adventure game.  
+ * "World of Zuul" is a very simple, text-based adventure game.  
  *
  * @author  Michael Kolling and David J. Barnes
  * @version 1.0 (February 2002)
  */
 
-public class Room 
-{
+public class Room {
     public String description;
     public Room northExit;
     public Room southExit;
     public Room eastExit;
     public Room westExit;
+    public Room upExit;
+    public Room downExit;
 
     /**
      * Create a room described "description". Initially, it has
      * no exits. "description" is something like "a kitchen" or
      * "an open court yard".
      */
-    public Room(String description) 
-    {
+    public Room(String description) {
         this.description = description;
     }
 
@@ -30,25 +30,20 @@ public class Room
      * Define the exits of this room.  Every direction either leads
      * to another room or is null (no exit there).
      */
-    public void setExits(Room north, Room east, Room south, Room west) 
-    {
-        if(north != null)
-            northExit = north;
-        if(east != null)
-            eastExit = east;
-        if(south != null)
-            southExit = south;
-        if(west != null)
-            westExit = west;
+    public void setExits(Room north, Room east, Room south, Room west, Room up, Room down) {
+        northExit = north;
+        eastExit = east;
+        southExit = south;
+        westExit = west;
+        upExit = up;
+        downExit = down;
     }
 
     /**
      * Return the description of the room (the one that was defined
      * in the constructor).
      */
-    public String getDescription()
-    {
+    public String getDescription() {
         return description;
     }
-
 }
